faaa301170c46542d1b18ab879e148709a192c04
dqxy <yanofsky.david@gmail.com>
2020-02-28 13:46:49 -0500

    React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from "react";
 import Header from "./components/Header.js";
+import  WelcomePage from './components/WelcomePage.js';
+import  CharacterList  from './components/CharacterList.js';
 
+import { Route } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 export default function App() {
   return (
     <main>
+      <div>
+     <div>
+      <div className="App">
+      <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+        <div>
+          <Link to="/">Welcome Page</Link>
+        </div>
+        <div>
+          <Link to="/CharacterList">Character List</Link>
+        </div>
+      </div>
+    </div>
+    <Route exact path="/" component={WelcomePage} />
+    <Route path="/CharacterList" component={CharacterList} />
+ 
+  </div>
       <Header />
     </main>
   );
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,83 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
+import styled from "styled-components";
+const Ss = styled.div`
+  height: 30px;
+  background: orange;
+  color: black;
+  border: 0;
+  margin: 5px 10px;
+`;
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  //const [pets, setPets] = useState([]);
+  //const [breed, setBreed] = useState("mix");
+
+  const [searchTerm, setSearchTerm] = useState("");
+  // searchResults is used to set the search result.
+  const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    axios
+    .get(`https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/`)
+    .then(response => {
+      const test = response.data.results;
+      var keyArray = test.map(function(item) { return item["name"]; });
+
+      console.log(keyArray);
+    
+
+
+      const results = keyArray.filter(character =>
+        character.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setSearchResults(results);
+
+
+
+
+    })
+  
+}, [searchTerm]);
+
+
+const handleChange = event => {
+  // console.log(event.target.value)
+  setSearchTerm(event.target.value);
+};
+
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+
+<form>
+     
+        <Ss>Check out these characters</Ss>
+        
+        <input
+          id="name"
+          type="text"
+          name="textfield"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleChange}
+        />
+      </form>
+
+      <div className="character-list">
+        <ul>
+          {searchResults.map(character => (
+            <li key={character}>{character}</li>
+          ))}
+        </ul>
+      </div>
+
+
+
     </section>
   );
 }
